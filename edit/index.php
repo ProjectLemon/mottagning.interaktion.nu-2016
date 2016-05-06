8fbe4948e935ad24773d6dea60cfacbb8a36736f
Fredrik Johansson <f95johansson@gmail.com>
2016-05-06 21:38:35 +0200

    Basic activity edit support with image upload

--- edit/index.php
+++ edit/index.php
@@ -5,6 +5,13 @@ $activites = json_decode(file_get_contents("content/activities.json"));
 if ($activites == null) {
     $activites = array();
 }
+$params = array_keys($_GET);
+if (count($params) != 1) {
+    $param = 'new';
+} else {
+    $param = $params[0];
+}
+$selected = false;
 
 ?>
 
@@ -16,19 +23,38 @@ if ($activites == null) {
   <body>
     <div class="wrapper">
       <form action="save.php" method="POST" enctype="multipart/form-data">
-        <select name="activity" class="activity-selector">
+        <select id="activity-select" name="activity">
+          <option value="New" required>New</option>
           <?php
             foreach ($activites as $activity) {
               $title = $activity->title;
-              echo "<option value=\"$title\">$title</option>\n";
+              if ($title == $param) {
+                $selected = $activity;
+                echo "<option selected value=\"$title\" required>$title</option>\n";
+              } else {
+                echo "<option value=\"$title\" required>$title</option>\n";
+              }
             }
           ?>
         </select>
         <br>
-        Titel:<input type="text" name="title"><br>
-        Bild:<input type="file" name="image">
+        Titel:<input type="text" name="title" required <?php if ($selected) echo 'value="'.$activity->title.'"' ?>>
+        <br>
+        Bild:<input type="file" name="image" required>
         <input type="submit" value="Spara">
       </form>
     </div>
+    
+    <script>
+      var select = document.getElementById('activity-select');
+      select.addEventListener('change', function changeActivity() {
+          var value = select.options[select.selectedIndex].value;
+          if (value == 'New') {
+            window.location.href = window.location.href.split('?')[0]
+          } else {
+            window.location.href = window.location.href.split('?')[0] + '?' + value;
+          }
+      });
+    </script>
   </body>
 </html>
--- edit/save.php
+++ edit/save.php
@@ -1,70 +1,121 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Edit: Response</title>
+    <meta charset="UTF-8">
+  </head>
+  <body>
 <?php
 
-function verifyUploadImage($image_name, $target_file) {
-    $valid = true;
-    $image_file_type = pathinfo($target_file, PATHINFO_EXTENSION);
+function verifyUploadImage($image_name, $target_dir) {
     
-    // Check if file already exists
-    for ($i = 0; $i < 7; $i++) {
-        
-        if (!file_exists($target_file)) {
-            $valid = true;
+    if (!isset($_FILES[$image_name]['tmp_name'])) {
+        throw new RuntimeException('No image provided');
+    }
+    $tmp_file_name = $_FILES[$image_name]['tmp_name'];
+    if (!file_exists($tmp_file_name) || $tmp_file_name == '') {
+        throw new RuntimeException('No image provided');
+    }
+    
+    // Check possible errors
+    switch ($_FILES[$image_name]['error']) {
+        case UPLOAD_ERR_OK:
             break;
-        }
-        
-        $valid = false;
-        sleep(1); // wait 1 second, max 7
+        case UPLOAD_ERR_NO_FILE:
+            throw new RuntimeException('No file sent.');
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            throw new RuntimeException('Exceeded filesize limit.');
+        default:
+            throw new RuntimeException('Unknown errors.');
     }
     
     // Check if image file is a actual image or fake image
-    if (isset($_POST["submit"])) {
-        $check = getimagesize($_FILES[$image_name]["tmp_name"]); // accepted hack to determine it's an image
+    if (isset($_POST['submit'])) {
+        $check = getimagesize($tmp_file_name); // accepted hack to determine it's an image
         
         if ($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
+            echo 'File is an image - ' . $check['mime'] . '.';
             
         } else {
-            echo "File is not an image.";
-            $valid = false;
+            throw new RuntimeException('File is not an image');
         }
     }
 
-    // Allow certain file formats
-    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-        echo "Sorry, only JPG, JPEG & PNG files are allowed.";
-        $valid = false;
-    } 
+    // Check MIME Type manually
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    $mime = $finfo->file($tmp_file_name, FILEINFO_MIME_TYPE);
+    if (!in_array(
+            $mime,
+            array(
+                'image/jpeg',
+                'image/jpg',
+                'image/png'
+            ), true)) { // strict
+
+        throw new RuntimeException('Sorry, only JPG, JPEG & PNG files are allowed.');
+    }
+    $extension = ($mime == 'image/png') ? '.png' : '.jpg';
     
-    return $valid;
-}
-
-
-
-
-$activites_file_name = "content/activities.json";
-$target_dir = "images/";
-$target_file = $target_dir . time();
-
-
-/* Varify and upload image */
-if (verifyUploadImage("image", $target_file)) {
     
-    // Try to upload file
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
+    // Check if target file name already exists
+    $exists = false;
+    for ($i = 0; $i < 7; $i++) {
+        $target_file = $target_dir . time() . $extension;
+        
+        if (!file_exists($target_file)) {
+            $exists = false;
+            break;
+        }
+        $exists = true;
+        sleep(1); // wait 1 second, max 7
+    }
+    if ($exists) {
+        throw new RuntimeException('Target file name already exist');
+    }
+
+    // You should also check filesize here. 
+    if ($_FILES[$image_name]['size'] > 5*1024*1024) { // 5mb
+        throw new RuntimeException('Exceeded filesize limit (5mb).');
     }
     
-} else {
-    echo "Sorry, your file was not uploaded.";
+    return $target_file;
+}
+
+function verifyForm() {
+    if (!isset($_POST['title']) || $_POST['title'] == '') {
+        throw new RuntimeException('No title was set');
+    }
 }
 
 
-/* Validate Form Data */
+// define ('SITE_ROOT', realpath(dirname(__FILE__))); // may need on server
+
+$activites_file_name = 'content/activities.json';
+$target_dir = 'content/images/';
+$image_file_key = 'image';
+
+
 try {
+    // Varify image
+    $target_file = verifyUploadImage($image_file_key, $target_dir);
+    
+    // Validate Form Data
+    verifyForm();
+    
+    
+    // Try to upload file
+    if (move_uploaded_file($_FILES[$image_file_key]['tmp_name'], $target_file)) {
+        echo '<p>The file '. basename( $_FILES[$image_file_key]['name']). ' has been uploaded.</p>';
+    } else {
+        throw new RuntimeException('Failed to move uploaded file.');
+    }
+    
+    
+    // All data should be in order, now save it
     $formdata = array(
 	    'title'=> $_POST['title'],
+	    'image'=> $target_file
     );
 
     // Open json db
@@ -78,14 +129,18 @@ try {
 	   
     // Save to json db file
     if (file_put_contents($activites_file_name, $jsondata)) {
-        echo 'Data successfully saved';
+        echo '<p>Data successfully saved</p>';
 	   
     } else {
-        echo "error";
+        throw new RuntimeException('Could not save to file');
     }
 
-} catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
+} catch (RuntimeException $e) {
+    echo '<h1>Error: ' . $e->getMessage() . '</h1>';
 }
 
 ?>
+
+    <a href="./">Back</a>
+  </body>
+</html>
