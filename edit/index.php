1f6b2d771e70079522161e517ea650bcb728e985
Fredrik Johansson <f95johansson@gmail.com>
2016-05-07 13:32:46 +0200

    All basic functionallity of edit page should now be working, except choosing location

--- edit/index.php
+++ edit/index.php
@@ -5,11 +5,10 @@ $activites = json_decode(file_get_contents("content/activities.json"));
 if ($activites == null) {
     $activites = array();
 }
-$params = array_keys($_GET);
-if (count($params) != 1) {
-    $param = 'new';
+if (isset($_GET['title'])) {
+    $param = $_GET['title'];
 } else {
-    $param = $params[0];
+    $param = 'Ny aktivitet';
 }
 $selected = false;
 
@@ -19,42 +18,112 @@ $selected = false;
   <head>
     <title>Title</title>
     <meta charset="UTF-8">
+    <style>
+      #image-upload-show {
+          width: 23em;
+          display: block;
+      }
+      #form-error {
+          display: none;
+      }
+    </style>
   </head>
   <body>
     <div class="wrapper">
-      <form action="save.php" method="POST" enctype="multipart/form-data">
+      <form id="edit-activity-form" action="edit.php" method="POST" enctype="multipart/form-data">
+          
         <select id="activity-select" name="activity">
-          <option value="New" required>New</option>
+          <option value="Ny aktivitet" required>Ny aktivitet</option>
           <?php
-            foreach ($activites as $activity) {
-              $title = $activity->title;
-              if ($title == $param) {
-                $selected = $activity;
-                echo "<option selected value=\"$title\" required>$title</option>\n";
-              } else {
-                echo "<option value=\"$title\" required>$title</option>\n";
-              }
+            foreach ($activites as $title => $activity) {
+                if ($title == $param) {
+                    $selected = $activity;
+                    echo "<option selected value=\"$title\" required>$title</option>\n";
+                } else {
+                    echo "<option value=\"$title\" required>$title</option>\n";
+                }
             }
           ?>
         </select>
+        <input type="submit" name="delete" value="Radera" 
+            <?php 
+              if (!$selected) { echo 'disabled';}
+            ?>>
         <br>
-        Titel:<input type="text" name="title" required <?php if ($selected) echo 'value="'.$activity->title.'"' ?>>
+        
+        Titel:<input type="text" name="title" required 
+            <?php
+              if ($selected && property_exists($selected, 'title')) echo 'value="'.$selected->title.'"'
+            ?>>
         <br>
-        Bild:<input type="file" name="image" required>
-        <input type="submit" value="Spara">
+        
+        <?php
+          if ($selected && property_exists($selected, 'image')) {
+            echo '<img id="image-upload-show" src="'.$selected->image.'">Ersätt';
+          }
+        ?>
+        Bild:<input id="image-upload" type="file" name="image" 
+            <?php
+              if (!$selected || ($selected && !property_exists($selected, 'image'))) {
+                echo 'required';
+              }
+            ?>>
+        <br>
+        
+        Beskrivning:<textarea name="description" rows="5" cols="30" required
+        ><?php if ($selected && property_exists($selected, 'description')) echo $selected->description
+            ?></textarea>
+        <br>
+        
+        Tid:<input type="text" name="time" required
+            <?php 
+              if ($selected && property_exists($selected, 'time')) echo 'value="'.$selected->time.'"'   
+            ?>>
+        <br>
+        
+        Datum:<input type="text" name="date" required
+            <?php 
+              if ($selected && property_exists($selected, 'date')) echo 'value="'.$selected->date.'"'   
+            ?>>
+            
+        <br>
+        <input type="submit" name="save" value="Spara">
+        <span id="form-error">Var snäll och fyll i hela formuläret</span>
       </form>
     </div>
     
     <script>
+      /* Reaload page with filled content when changing activity */
       var select = document.getElementById('activity-select');
-      select.addEventListener('change', function changeActivity() {
+      select.addEventListener('change', function() {
           var value = select.options[select.selectedIndex].value;
-          if (value == 'New') {
-            window.location.href = window.location.href.split('?')[0]
+          if (value == 'Ny aktivitet') {
+              window.location.href = window.location.href.split('?')[0]
           } else {
-            window.location.href = window.location.href.split('?')[0] + '?' + value;
+              window.location.href = window.location.href.split('?')[0] + '?title=' + encodeURIComponent(value);
           }
       });
+      
+      /* Preivew image before uploading */
+      var imageUpload = document.getElementById('image-upload');
+      imageUpload.addEventListener('change', function changeActivity(event) {
+          var imageUploadShow = document.getElementById('image-upload-show');
+          if (imageUploadShow) {
+              imageUploadShow.src = URL.createObjectURL(event.target.files[0]);
+          }
+      });
+      
+      /* Add cross-browser support for required */
+      var form = document.getElementById('edit-activity-form');
+      form.noValidate = true;
+      form.addEventListener('submit', function(event) {
+          if (!event.target.checkValidity()) {
+              event.preventDefault();
+              formError = document.getElementById('form-error');
+              formError.style.display = 'inline';
+              window.setTimeout(function(){ formError.style.display = 'none'; }, 4000); // 4 seconds
+          }
+      }, false);
     </script>
   </body>
 </html>
