c27d61e17f3468aa1222f0581f87a1d841406f3c
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2016-05-24 17:44:06 +0200

    minor changes

--- src/js/fonts.js
+++ src/js/fonts.js
@@ -4,7 +4,7 @@
 * Last      Modified: 2016-05-17
 * Purpose:  This file is used to download external fonts to the site. This is
 *           done through JavaScript on order to be able to load it after all
-*           the page's content has loaded. And also to allow thos resource to
+*           the page's content has loaded. And also to allow those resources to
 *           be downloaded using it's own, separate thread.
 */
 
